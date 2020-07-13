5310b4a632dfe275c300446795a1e386e0b6648b
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-07-13 11:25:18 +0100

    building up homepage~~pure js, skeleton css used

---
+++ client/client.js
@@ -0,0 +1,25 @@
+console.log("Hello");
+
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit', (event) => {
+    //let's js handle the data
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const mew = {
+        name,
+        content
+    };
+    console.log(mew);
+
+    form.style.display = 'none';
+
+    loadingElement.style.display = '';
+
+});
